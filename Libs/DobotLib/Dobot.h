4eeb7fc440cb50f5c56171f75f8de055928888d6
Abdellah-selatnia <selatniamohamedabdellah@gmail.com>
2025-06-24 11:15:20 +0200

    calibration
    modifications des positions X,Y,Z

--- Libs/DobotLib/Dobot.h
+++ Libs/DobotLib/Dobot.h
@@ -5,36 +5,36 @@
 #include "Command.h"
 #include <stdio.h>
 
-#define X1 91.3997 //X-coordinate of Apoint
-#define Y1 259.5008//Y-coordinate of A point
-#define Z1 80.7794 //Z-coordinate of A point
-#define R1 170.597 //R-coordinate of A point
+#define X1 -71.14275 //X-coordinate of A point
+#define Y1 215.7542//Y-coordinate of A point
+#define Z1 46.6987//Z-coordinate of A point
+#define R1 108.3176 //R-coordinate of A point
 
 
-#define X2 90.1254//X-coordinate of B point
-#define Y2 258.5482//Y-coordinate of B point
-#define Z2 49.657 //Z-coordinate of B point
-#define R2 170.7823 //R-coordinate of B point
+#define X2 -73.1375//X-coordinate of B point
+#define Y2 212.3363//Y-coordinate of B point
+#define Z2 30.5996 //Z-coordinate of B point
+#define R2 109.0059 //R-coordinate of B point
 
-#define X3 142.4404 //X-coordinate of C point
-#define Y3 179.9995 //Y-coordinate of C point
-#define Z3 73.8378 //Z-coordinate of C point
-#define R3 151.6441 //R-coordinate of C point
+#define X3 1.539 //X-coordinate of C point
+#define Y3 195.9458 //Y-coordinate of C point
+#define Z3 44.054 //Z-coordinate of C point
+#define R3 89.55 //R-coordinate of C point
 
-#define X4 143.6304 //X-coordinate of D point
-#define Y4 -216.6878 //Y-coordinate of D point
-#define Z4 78.1144 //Z-coordinate of D point
-#define R4 43.5382 //R-coordinate of d point
+#define X4 246.9173 //X-coordinate of D point
+#define Y4 -103.4815 //Y-coordinate of D point
+#define Z4 27.2219 //Z-coordinate of D point
+#define R4 -22.7383 //R-coordinate of d point
 
-#define X5 299.4815 //X-coordinate of E point
-#define Y5 -74.8326 //Y-coordinate of E point
-#define Z5 52.4756 //Z-coordinate of E point
-#define R5 85.9706//R-coordinate of E point
+#define X5 260.5969 //X-coordinate of E point
+#define Y5 137.5167 //Y-coordinate of E point
+#define Z5 28.7954 //Z-coordinate of E point
+#define R5 27.8206//R-coordinate of E point
 
-#define X6 225.5811 //X-coordinate of F point
-#define Y6 51.738 //Y-coordinate of F point
-#define Z6 75.3284 //Z-coordinate of F point
-#define R6 112.9176//R-coordinate of F point
+#define X6 115.8233 //X-coordinate of F point
+#define Y6 193.5941 //Y-coordinate of F point
+#define Z6 105.6224 //Z-coordinate of F point
+#define R6 59.1088//R-coordinate of F point
 
 
 typedef enum tagIOFunction {
